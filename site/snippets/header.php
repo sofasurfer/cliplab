ce5c1b00ceaa5ba0defbd23e1e23d2f6046c845e
kib <kib@sofabook.local>
2017-10-21 17:31:22 +0200

    Change bootstrap to CND

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -8,8 +8,8 @@
   <title><?= $site->title()->html() ?> | <?= $page->title()->html() ?></title>
   <meta name="description" content="<?= $site->description()->html() ?>">
 
-  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet"  />   
-  <?= css('assets/css/bootstrap.min.css') ?>
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet"  /> 
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
   <?= css('assets/css/jquery.fullpage.min.css') ?>
   <?= css('assets/css/index.css?v1.1') ?>
 
