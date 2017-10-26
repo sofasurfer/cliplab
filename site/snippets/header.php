615300547b9d48aaffbb319b6f56bd7e1b3f0d4a
sofasurfer <kilian@sofasurfer.ch>
2017-10-26 14:36:31 +0200

    Fix responsive fonts on home

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -11,7 +11,7 @@
   <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet"  /> 
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
   <?= css('assets/css/jquery.fullpage.min.css') ?>
-  <?= css('assets/css/index.css?v1.1') ?>
+  <?= css('assets/css/index.css?v1.2') ?>
 
 </head>
 <body class="<?= $page->intendedTemplate(); ?>">
